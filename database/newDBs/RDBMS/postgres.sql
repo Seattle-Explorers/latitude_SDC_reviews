52b5198efb9b03d7d24cdc4c64d8fbef1dd60ddc
Won Gyun Park <park.wongyun@gmail.com>
2020-07-16 05:53:42 -0700

    Change Cassandra Schema

--- database/newDBs/RDBMS/postgres.sql
+++ database/newDBs/RDBMS/postgres.sql
@@ -19,19 +19,19 @@ CREATE TABLE posts (
 );
 
 CREATE TABLE reviews (
-  id VARCHAR(10) PRIMARY KEY,
-  reviewer_name VARCHAR(255) NOT NULL,
-  body VARCHAR(1000) NOT NULL,
-  date VARCHAR(255) NOT NULL,
-  dp VARCHAR(5) NOT NULL,
-  response VARCHAR(1000),
+  reviewId VARCHAR(10) PRIMARY KEY,
+  reviewerDp VARCHAR(5) NOT NULL,
+  reviewerName VARCHAR(255) NOT NULL,
+  reviewBody VARCHAR(1000) NOT NULL,
+  reviewDate VARCHAR(255) NOT NULL,
+  reviewResponse VARCHAR(1000),
   listingId VARCHAR(10) NOT NULL REFERENCES posts(paddedId)
 );
 
-COPY posts (id,paddedId,locationAvg,valueAvg,accuracyAvg,commAvg,cleanAvg,checkinAvg,avg,username,userDp) FROM '/home/wil/Desktop/hackreactor/SDC/latitude_SDC_will/database/newDBs/postsData.csv' DELIMITER ',' CSV HEADER;
+-- COPY posts (id,paddedId,locationAvg,valueAvg,accuracyAvg,commAvg,cleanAvg,checkinAvg,avg,username,userDp) FROM '/home/wil/Desktop/hackreactor/SDC/latitude_SDC_will/database/newDBs/postsData.csv' DELIMITER ',' CSV HEADER;
 
-COPY reviews (id,reviewer_name,body,date,dp,response,listingId) FROM '/home/wil/Desktop/hackreactor/SDC/latitude_SDC_will/database/newDBs/reviewsData.csv' DELIMITER ',' CSV HEADER;
+-- COPY reviews (id,reviewer_name,body,date,dp,response,listingId) FROM '/home/wil/Desktop/hackreactor/SDC/latitude_SDC_will/database/newDBs/reviewsData.csv' DELIMITER ',' CSV HEADER;
 
-CREATE INDEX reviews_listingId_idx ON reviews (listingId);
+-- CREATE INDEX reviews_listingId_idx ON reviews (listingId);
 
-CREATE INDEX posts_paddedId_idx ON posts (paddedId);
+-- CREATE INDEX posts_paddedId_idx ON posts (paddedId);
